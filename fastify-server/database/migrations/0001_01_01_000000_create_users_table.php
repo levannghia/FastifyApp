9d027052c1d935ed44e591e72d59327bded89b55
levannghia <levannghia.tdc2018@gmail.com>
2024-09-10 13:03:17 +0700

    Creat Branch Table

--- fastify-server/database/migrations/0001_01_01_000000_create_users_table.php
+++ fastify-server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('phone');
+            $table->json('live_location')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['Customer', 'Admin', 'DeliveryPartner'])->comment("Customer, Admin, DeliveryPartner")->default('Customer');
+            $table->string('address')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
