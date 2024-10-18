1a3313b422705ea71b7b79b900e3922603cf09f5
levannghia <levannghia.tdc2018@gmail.com>
2024-10-18 14:21:30 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Spatie\Async\Pool;
+use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -12,6 +14,26 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
+});
+
+Route::get('/test', function () {
+    $pool = Pool::create();
+    $counter = 0;
+
+    foreach (range(1, 5) as $i) {
+        $pool[] = async(function () use ($i) {
+            usleep(random_int(1000, 10000));
+            Log::info("Task {$i} completed."); // Ghi log khi tác vụ hoàn thành
+            return 2;
+        })->then(function (int $output) use (&$counter, $i) {
+            $counter += $output;
+            Log::info("Task {$i} returned value: {$output}"); // Ghi log giá trị trả về
+        });
+    }
+
+    await($pool);
+    Log::info("Total counter value: {$counter}"); // Ghi log tổng giá trị
+    return "The counter value is: " . $counter;
 });
 
 Route::get('/dashboard', function () {
@@ -24,4 +46,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
