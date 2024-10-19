9559d2641b334cf2ead19cccaeb820fe3b58924d
levannghia <levannghia.tdc2018@gmail.com>
2024-10-19 09:01:35 +0700

    Len

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,6 +3,10 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+// Enable credentials and XSRF token handling
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
--- routes/channels.php
+++ routes/channels.php
@@ -9,7 +9,7 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('confirm.order.{id}', function(int $id){
+Broadcast::channel('confirm.order.{id}', function($user, int $id){
     $order = Order::find($id);
     if($order) {
         return true;
